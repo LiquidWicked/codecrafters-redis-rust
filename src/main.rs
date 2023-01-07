2006e386d2c07b04a676819b30c77373e9015170
Kresimir Bruner <kresimir.bruner@gmail.com>
2023-01-07 18:42:44 +0100

    Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::Write, net::{TcpListener, TcpStream}};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,12 +8,18 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                respond(stream);
+
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn respond(mut stream: TcpStream) {
+    stream.write(b"+PONG\r\n").unwrap();
 }
